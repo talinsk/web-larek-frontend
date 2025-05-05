88ec90bc10676373415c69c5f14a049481e8ca46
Natalia <tali154@yandex.ru>
2025-05-05 13:11:21 +0700

    changed types, separated orderInfo interface, added cart implementation

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { IEvents } from "../components/base/events";
+
 export interface IProduct {
     id: string;
     description: string;
@@ -11,21 +13,73 @@ export interface ICart {
     products: IProduct[];
     count: number;
     cost: number;
-    toggleProduct(product: IProduct): void;
+    addProduct(product: IProduct): void;
+    removeProduct(product: IProduct): void;
     clear(): void;
 }
 
 export type TPaymentType = 'online' | 'deliver';
 
-export interface IOrderInfo {
+export interface IDeliveryInfo {
     paymentType: TPaymentType;
     address: string;
+}
+
+export interface ICustomerContact {
     email: string;
     phone: string;
+}
+
+export interface IOrderInfo extends IDeliveryInfo, ICustomerContact {
     total: number;
 }
 
 export interface IOrderResult {
     id: string;
     total: number;
+}
+
+
+// имплементация интерфейсов
+export class Cart implements ICart {
+    products: IProduct[];
+
+    constructor(protected events: IEvents) {
+    }
+
+    get count(): number {
+        return this.products.length;
+    }
+
+    get cost(): number {        
+        return this.products.reduce(
+            (accumulator, currentValue) => accumulator + (currentValue.price || 0),
+            0,
+        );
+    }
+
+    addProduct(product: IProduct): void {
+        const index = this.products.findIndex(p => p.id == product.id);
+        if (index < 0) {
+            this.products.push(product);
+            this.emitChangeCart();
+        }
+    }
+    
+    removeProduct(product: IProduct): void {
+        const index = this.products.findIndex(p => p.id == product.id);
+        if (index >= 0) {
+            this.products.splice(index, 1);
+            this.emitChangeCart();
+        }
+    }
+
+    clear(): void {
+        this.products = [];
+        this.emitChangeCart();
+    }
+
+    private emitChangeCart() {
+        this.events.emit('cart:change', {});
+    }
 }
